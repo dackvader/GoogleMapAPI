20745dc3c65623bf46d6cbc8cf15b4232d320ff5
dackvader <dackvader@outlook.com>
2023-01-23 11:37:20 +0700

    remove_JSON

--- geocoding01.js
+++ geocoding01.js
@@ -78,8 +78,8 @@ function geocode(request) {
       marker.setPosition(results[0].geometry.location);
       marker.setMap(map);
       responseDiv.style.display = "block";
-      response.innerText = JSON.stringify(result, null, 2);
-      return results;
+//      response.innerText = JSON.stringify(result, null, 2);
+//      return results;
     })
     .catch((e) => {
       alert("Geocode was not successful for the following reason: " + e);
